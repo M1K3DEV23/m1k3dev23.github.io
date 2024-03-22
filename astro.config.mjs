08a3b7711b3757fa0e63996eb3cfa1920c5d647b
M1K3DEV23 <mikery2310@outlook.com>
2024-03-22 15:17:40 -0600

    agregando lo necesario

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'https://m1k3dev23.github.io/Asian_food',
+  site: 'https://astronaut.github.io',
 });
