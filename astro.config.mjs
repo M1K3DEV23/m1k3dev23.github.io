8a0150d634d204df5d72a1c09752a22096422d22
M1K3DEV23 <mikery2310@outlook.com>
2024-03-22 15:06:41 -0600

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://M1K3DEV23.github.io',
   base: 'https://m1k3dev23.github.io/Asian-food/',
 });
